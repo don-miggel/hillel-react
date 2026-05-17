a37af1b94d1beda2f18cacc1db68f4c0eaecedab
mykhailo_derecha <don.miggel@yahoo.com>
2026-05-17 15:06:38 +0300

    MovieCard component refactored

--- homework_02/components/MovieCard.jsx
+++ homework_02/components/MovieCard.jsx
@@ -2,12 +2,13 @@ import Button from "./Button";
 
 const MovieCard=({movie, setSelectedMovie}) =>{
     return (
-        <div className="movie-card">
+        <>
           <span><b>{movie.title}</b> ({movie.year})</span>
           <span> — {movie.genre}, </span>
           <span>⭐ {movie.rating} </span>
           <Button movie={movie} setSelectedMovie={setSelectedMovie} />
-        </div>);
+        </>
+    );
 }
 
 export default MovieCard;
