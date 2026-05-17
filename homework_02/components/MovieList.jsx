e940f059a47e88ab2b29f13246b0db927fa35430
mykhailo_derecha <don.miggel@yahoo.com>
2026-05-17 13:46:21 +0300

    Movie List homework completed

---
+++ homework_02/components/MovieList.jsx
@@ -0,0 +1,6 @@
+
+const MovieList = (movies) =>{
+
+}
+
+export default MovieList
---
+++ homework_02/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+const movies = [
+  { id: 1, title: 'Inception', year: 2010, genre: 'Sci-Fi', rating: 8.8 },
+  { id: 2, title: 'The Dark Knight', year: 2008, genre: 'Action', rating: 9.0 },
+  { id: 3, title: 'Interstellar', year: 2014, genre: 'Sci-Fi', rating: 8.6 },
+  { id: 4, title: 'Parasite', year: 2019, genre: 'Thriller', rating: 8.5 },
+  { id: 5, title: 'The Matrix', year: 1999, genre: 'Sci-Fi', rating: 8.7 },
+  { id: 6, title: 'Joker', year: 2019, genre: 'Drama', rating: 8.4 },
+  { id: 7, title: 'Avengers: Endgame', year: 2019, genre: 'Action', rating: 8.4 },
+  { id: 8, title: 'Whiplash', year: 2014, genre: 'Drama', rating: 8.5 },
+];
+
+
+function App() {
+  const [movies, setMovies] = useState(movies);
+  const[selectedMovie , setSelectedMovie] = useState(null);
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
