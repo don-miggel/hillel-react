882e124849753a6c2da07d8a98edfed3d25c618c
mykhailo_derecha <don.miggel@yahoo.com>
2026-06-07 20:39:37 +0300

    Comments removed in the App.jsx, unused code removed in the main.jsx and back navigation link was added into a LocationDetailsRoute component

--- homework_06/src/App.jsx
+++ homework_06/src/App.jsx
@@ -1,25 +1,11 @@
-import { useState } from 'react'
-import { useEffect } from 'react';
-import { service as locationService } from './service/locations';
 import { RouterProvider } from "react-router";
 import { router } from './router/router';
 
 function App() {
 
- // const [locations, setLocations] = useState([]);
-
- // useEffect(()=>{
-  //  const fetch = async () => {locationService.get()
-  //    .then(data =>setLocations(data));
-  //  } 
-  //  fetch();
- // }, [])
-  
-
   return (
     <>
       < RouterProvider router={router}/>
-
     </>
   )
 }
--- homework_06/src/routes/LocationDetailsRoute.jsx
+++ homework_06/src/routes/LocationDetailsRoute.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
-import { useLoaderData } from 'react-router'
+import { useLoaderData, useNavigate } from 'react-router'
 
 export default function LocationDetailsRoute() {
 
     const location = useLoaderData();
-  return (
-    <>
-        <h1>Location Details Route Title</h1>
-    
-        <p><b>Name: </b> {location.name}</p>
-        <p><b>Address: </b>: {location.address}</p>
-        <p><b>Type: </b> {location.type}</p>
-        <p><b>Rating: </b>{location.rating}</p>
+    const navigate = useNavigate();
 
-    </>
+    return (
+        <>
+            <h1>Location Details Route Title</h1>
+        
+            <p><b>Name: </b> {location.name}</p>
+            <p><b>Address: </b>: {location.address}</p>
+            <p><b>Type: </b> {location.type}</p>
+            <p><b>Rating: </b>{location.rating}</p>
+            <hr />
+            <div onClick={()=> navigate(-1)}> <u>Back to Locations</u></div>
+
+        </>
   )
 }
